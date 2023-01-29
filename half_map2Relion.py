f9b76d5e007ab01ba908669264dc2b67ca8633fb
Koji Takaki <pr5r1uqr@s.okayama-u.ac.jp>
2023-01-29 15:38:36 +0900

    added some annotation

--- half_map2Relion.py
+++ half_map2Relion.py
@@ -23,16 +23,20 @@ def reedfile():
         direc_sharp = direc + "/*sharp.mrc"
         direc_half = direc + "/**"
         sharp_map = glob.glob(direc_sharp)
+        #if selected direcotry doesn't contain refined mrc map
         if not sharp_map:
             print(
                 "couldn't find \"volume_map_sharp.mrc\", please make sure you select Job from Homogeneous Refinement")
             messagebox.showerror("cound't find file !!",
                                  "couldn't find \"volume_map_sharp.mrc\", please make sure you select Job from Homogeneous Refinement")
+
+        #if it contains refined map
         if sharp_map:
             print("loaded successfully!" + " :" + str(sharp_map))
             half_map = [p for p in glob.glob(direc_half, recursive=True)
                         if re.search("map_half", p)]
             messagebox.showinfo("Loaded Successfully!", "Loaded Successfully!!")
+            #return list stored two path(both half map)
             return half_map
             break
 
@@ -40,18 +44,21 @@ def copy2relion(half_map):
     # hide root window
     root = tkinter.Tk()
     root.withdraw()
-    # select directory
+    # select destination
     messagebox.showinfo("Select destination", "Please select directory currently processing on Relion")
     destination = filedialog.askdirectory()
     destination_A = destination + "/run_half1_class001_until.mrc"
     destination_B = destination + "/run_half2_class001_until.mrc"
+
+    #Copy half map from half_map and rename it for relion to destination path
     shutil.copy(half_map[0], destination_A)
-    print("copied " + half_map[0] + "→" + "to" + destination + "and renamed to" + destination_A)
+    print("copied " + half_map[0] + " →→→→→" + "to " + destination + " and renamed to " + destination_A)
     shutil.copy(half_map[1], destination_B)
-    print("copied " + half_map[1] + "→" + "to" + destination + "and renamed to" + destination_B)
+    print("copied " + half_map[1] + " →→→→→ " + "to " + destination + " and renamed to " + destination_B)
 
-
+#main process
 half_map = reedfile()
 copy2relion(half_map)
 
+print("done")
 print("プログラムを終了しています")
